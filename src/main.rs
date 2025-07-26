1ce5d52402f7e03e25ee0fc4fdaa341c0e91d2f2
Steve Cirelli <scirelli+git@gmail.com>
2025-07-25 20:11:17 -0400

    Learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello world!");
+    let k = first_word(&s);
+    println!("{k}");
+
+    let m = "New word";
+    let l = first_word(&m);
+    println!("{l}");
+}
+
+fn first_word(s: &str) -> &str {
+    for (i, &item) in s.as_bytes().iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    s
 }
